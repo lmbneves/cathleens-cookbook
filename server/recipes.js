cb40b92a9fef2ad44cb0ec887db2ef2a72da07fd
lmbneves <lmbneves@gmail.com>
2020-12-20 13:40:19 -0500

    simple seed

--- server/recipes.js
+++ server/recipes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Recipe = require('./Recipe');
+const Recipe = require('./recipe');
 
 router.get('/', async (req, res) => {
   try {
--- server/server.js
+++ server/server.js
@@ -12,6 +12,31 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to database!'));
 
+const Recipe = require('./recipe');
+async function mySeeder() {
+  const data = await Recipe.find({}).exec();
+  if (data.length !== 0) {
+      // Data exists, no need to seed.
+      return;
+  }
+  const seed = new Recipe({
+    "title": "A pizza recipe",
+    "prep_time": "20 minutes",
+    "cook_time": "5 minutes",
+    "servings": "2",
+    "description": "This is a recipe for a yummy pizza",
+    "ingredients": "ingredients",
+    "directions": "directions"
+  });
+
+  // some other seed logic
+  // ...
+
+  await seed.save()
+}
+
+mySeeder();
+
 const router = require('./recipes');
 server.use('/recipes', router);
 
