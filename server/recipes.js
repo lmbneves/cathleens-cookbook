6f012c74ed9e6810827550170acf0e13589f5ff0
lmbneves <lmbneves@gmail.com>
2020-12-20 18:06:50 -0500

    flush out recipe API with GET and POST logic

--- server/recipes.js
+++ server/recipes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Recipe = require('./recipe');
 
+// GET all recipes
 router.get('/', async (req, res) => {
   try {
     const recipes = await Recipe.find();
@@ -11,6 +12,12 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET one recipe
+router.get('/:id', getRecipe, (req, res) => {
+  res.json(res.recipe);
+})
+
+// POST create one recipe
 router.post('/', async (req, res) => {
   const recipe = new Recipe({
     title: req.body.title,
@@ -21,6 +28,7 @@ router.post('/', async (req, res) => {
     ingredients: req.body.ingredients,
     directions: req.body.directions
   });
+
   try {
     const newRecipe = await recipe.save();
     res.status(201).json(newRecipe);
@@ -28,4 +36,29 @@ router.post('/', async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+// PATCH update one recipe
+router.patch('/:id', getRecipe, (req, res) => {
+  
+});
+
+// DELETE remove one recipe
+router.delete('/:id', getRecipe, (req, res) => {
+  
+});
+
+async function getRecipe(req, res, next) {
+  try {
+    recipe = await Recipe.findById(req.params.id)
+    if (recipe == null) {
+      return res.status(404).json({ message: 'Cant find recipe'})
+    }
+  } catch(err){
+    return res.status(500).json({ message: err.message })
+  }
+
+  res.recipe = recipe
+  next()
+}
+
 module.exports = router;
