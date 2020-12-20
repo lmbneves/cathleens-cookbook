afb6cc74f35f92c2c86c9fbf94bba5c24cb699da
lmbneves <lmbneves@gmail.com>
2020-12-20 15:10:19 -0500

    space out recipe list heading

--- client/src/components/RecipeList.vue
+++ client/src/components/RecipeList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid class="recipes-container">
-    <h1 align="center">What's on the menu?</h1>
+    <h1 align="center" class="mt-10 mb-16">What's on the menu?</h1>
     <v-container>
       <v-layout>
         <v-flex d-flex xs12 sm6 md4>
