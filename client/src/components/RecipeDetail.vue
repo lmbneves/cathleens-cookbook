832b53b466e9f4c8162790b3cba0636431510777
lmbneves <lmbneves@gmail.com>
2020-12-21 11:08:47 -0500

    flush out recipe detail style a bit more

--- client/src/components/RecipeDetail.vue
+++ client/src/components/RecipeDetail.vue
@@ -12,25 +12,37 @@
         <v-spacer></v-spacer>
         <v-col
           cols="3">
-          <div class="pa-4 pb-1 recipe-stats">
-            <p>Prep time: {{ recipe.prep_time }}</p>
-            <p>Cook time: {{ recipe.cook_time }}</p>
-            <p>Serves: {{ recipe.servings }}</p>
+          <div class="pt-6 pb-2 pl-6 pr-6 recipe-stats">
+            <p>
+              <v-icon class="recipe-stats__icon">mdi-alarm-check</v-icon>
+              <span class="recipe-stats__content">{{ recipe.prep_time }}</span>
+            </p>
+            <p>
+              <v-icon class="recipe-stats__icon">mdi-pot-steam</v-icon>
+              <span class="recipe-stats__content">{{ recipe.cook_time }}</span>
+            </p>
+            <p>
+              <v-icon class="recipe-stats__icon">mdi-account-multiple</v-icon>
+              <span class="recipe-stats__content">{{ recipe.servings }}</span>
+            </p>
           </div>
         </v-col>
         <v-spacer></v-spacer>
         <v-col
           cols="7">
-          <h1>{{ recipe.title }}</h1>
-          <h3>{{ recipe.description }}</h3>
+          <h1 class="mb-2 recipe-title">{{ recipe.title }}</h1>
+          <h3 class="recipe-subtitle">{{ recipe.description }}</h3>
         </v-col>
+      </v-row>
+      <v-row>
+        <div style="height: 20px;"></div>
       </v-row>
       <v-row>
         <v-spacer></v-spacer>
         <v-col
           cols="3">
           <h2 class="pb-4">Ingredients</h2>
-          <p>{{ recipe.ingredients }}</p>
+          <p class="recipe-ingredients__content">{{ recipe.ingredients }}</p>
         </v-col>
         <v-spacer></v-spacer>
         <v-col
@@ -69,9 +81,34 @@
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css?family=Lobster');
+.recipe-title {
+  font-family: 'Lobster', sans-serif;
+  font-size: 42pt;
+  word-spacing: 3pt;
+}
+
+.recipe-subtitle {
+  font-size: 14pt;
+  font-weight: 300;
+}
 
 .recipe-stats {
-  background-color: grey;
+  background-color: #B7B8CA;
+  border-radius: 2px;
+  font-weight: 700;
+}
+
+.recipe-stats__icon {
+  margin-top: -4px;
+}
+
+.recipe-stats__content {
+  display: inline-block;
+  margin-left: 8px;
+}
+
+.recipe-ingredients__content {
+  min-height: 200px;
+  border-right: 1px solid #B7B8CA;
 }
 </style>
