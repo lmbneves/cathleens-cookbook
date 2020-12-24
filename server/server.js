773d872836009713bbc4c31f80eae90856020cc4
lmbneves <lmbneves@gmail.com>
2020-12-24 14:43:57 -0500

    configure prod db setup

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ server.use(express.json());
 server.use(cors());
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to database!'));
