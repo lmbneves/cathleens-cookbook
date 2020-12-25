ee54047f32b37ae56630acc39b36c683e2145456
lmbneves <lmbneves@gmail.com>
2020-12-24 21:43:50 -0500

    revert db_uri

--- server/server.js
+++ server/server.js
@@ -7,8 +7,7 @@ server.use(express.json());
 server.use(cors());
 
 const mongoose = require('mongoose');
-const db_uri = process.env.NODE_ENV === "prod" ? process.env.MONGODB_URI : process.env.DATABASE_URL;
-mongoose.connect(db_uri, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to database!'));
